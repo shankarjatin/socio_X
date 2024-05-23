0b68f1aeaa011881f6d0eb3b125b6a30078c9653
shankarjatin <shankarjatin1005@gmail.com>
2024-05-23 11:49:52 +0530

    mvc

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+app.use("/api/auth", authRoutes)
 app.listen( 8000 ,()=>{
     console.log("server is running on port 8000")
 })
