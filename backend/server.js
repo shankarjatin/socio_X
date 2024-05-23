3672e31e2d07257d2881f772c9af3d028e2c4fe3
shankarjatin <shankarjatin1005@gmail.com>
2024-05-23 11:46:24 +0530

    backend connfig

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen( 8000 ,()=>{
+    console.log("server is running on port 8000")
+})
